c33cf9e72a31b0c74217b0cf014ba1722590efe3
null <null>
2025-11-23 14:30:03 -0600

    build fix2

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Esta configuración es vital para que Vercel entienda tu App
-export default defineConfig({
-  plugins: [react()],
-})
